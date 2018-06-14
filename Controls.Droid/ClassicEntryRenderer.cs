9422e933327a874915cbe63f6f24c4d35489c422
Bugger <skordesign@outlook.com>
2018-06-14 16:56:58 +0700

    update

---
+++ Controls.Droid/ClassicEntryRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Skor.Controls.Droid.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Skor.Controls.Droid
+{
+    public class ClassicEntryRenderer: EntryRenderer
+    {
+        private ClassicEntry classicEntry;
+        public ClassicEntryRenderer(Context context):base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+                classicEntry = e.NewElement as ClassicEntry;
+            Control.Background = CreateBackground();
+        }
+
+        private Drawable CreateBackground()
+        {
+            var normalDrawable = BackgroundExtension.CreateBackgroundGradient(classicEntry.BackgroundColor.ToAndroid(),
+                classicEntry.BackgroundColor.ToAndroid(),
+                classicEntry.BackgroundColor.ToAndroid(),
+                (float)classicEntry.CornerRadius,
+                GradientDrawable.Orientation.BlTr);
+            var statesListDrawable = new StateListDrawable();
+            //statesListDrawable.AddState(new int[] { -Android.Resource.Attribute.StateEnabled }, layerDisabled);
+            //statesListDrawable.AddState(new int[] { Android.Resource.Attribute.StatePressed }, layer);
+            //statesListDrawable.AddState(new int[] { Android.Resource.Attribute.StateEnabled }, layer);
+            return statesListDrawable;
+        }
+    }
+}
---
+++ Controls/ClassicEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Skor.Controls
+{
+    public class ClassicEntry:Entry
+    {
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(
+      nameof(CornerRadius),
+      typeof(double),
+      typeof(ClassicEntry),
+      0d);
+        public double CornerRadius
+        {
+            get { return (double)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+    }
+}
